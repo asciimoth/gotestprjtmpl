a5f76719a355c5406db8324da16effd5357c415e
AsciiMoth <ascii@moth.contact>
2025-09-05 00:45:11 +0400

    ci: add nix flake

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Go test project template";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    gomod2nix = {
+      url = "github:tweag/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+    gomod2nix,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ gomod2nix.overlays.default ];
+      };
+
+      app = pkgs.buildGoApplication {
+        name = "gotestprjtmpl";
+        # version = "0.1.0";
+        src = ./.;
+        modules = ./gomod2nix.toml;
+        # buildInputs = with pkgs; [ ];
+      };
+
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            govet.enable = true;
+            gomod2nix = {
+              enable = true;
+              description = "TODO";
+              entry = "echo AAAAAAA";
+              stages = [ "pre-commit" ];
+            };
+          };
+        };
+      };
+    in {
+      devShell = pkgs.mkShell {
+        inherit (checks.pre-commit-check) shellHook;
+        buildInputs = with pkgs; [
+          go
+          commitizen
+          goreleaser
+          git-cliff
+
+          gomod2nix.packages.${system}.default
+        ];
+      };
+
+      defaultPackage = app;
+    });
+}
