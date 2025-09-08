6924e7565eaad5ca4a180dadd848c0e0c5a1290d
AsciiMoth <ascii@moth.contact>
2025-09-08 11:17:07 +0400

    feat: add basic http example

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com
 
 go 1.24.6
+
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -3,8 +3,34 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
 )
 
+func err(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func hello(w http.ResponseWriter, _ *http.Request) {
+	_, e := fmt.Fprintf(w, "hello\n%s\n", uuid.New())
+	err(e)
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			_, e := fmt.Fprintf(w, "%v: %v\n", name, h)
+			err(e)
+		}
+	}
+}
+
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	err(http.ListenAndServe(":8090", nil)) //nolint:gosec
 }
